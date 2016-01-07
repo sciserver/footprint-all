cadd6b453544782fc2f84233e9dc94aca23b906c
Laszlo Dobos <dobos@complex.elte.hu>
2016-01-07 17:26:39 +0100

    Initial git check-in, setting up necessary submodules.

---
+++ checkout.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#git clone --recursive --branch v2.0/develop git@github.com:sciserver/footprint-all.git
+
+cd graywulf
+git checkout -B v1.1/develop remotes/origin/v1.1/develop --
+
+cd ../graywulf-plugins
+git checkout -B v1.1/develop remotes/origin/v1.1/develop --
+
+cd ../footprint
+git checkout -B v2.0/develop remotes/origin/v2.0/develop --
+
+cd ../footprint-config
+git checkout -B v2.0/develop remotes/origin/v2.0/develop --
+
+cd ../spherical
+git checkout -B develop remotes/origin/develop --
+
+cd ..
