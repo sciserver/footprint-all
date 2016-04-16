f6536fd9913bb9955860b845a8720697774377bd
Laszlo Dobos <dobos@complex.elte.hu>
2016-04-16 10:54:55 +0200

    Entities submodule added.

--- checkout.sh
+++ checkout.sh
@@ -7,6 +7,9 @@ git checkout -B v1.1/develop remotes/origin/v1.1/develop --
 cd ../graywulf-plugins
 git checkout -B v1.1/develop remotes/origin/v1.1/develop --
 
+cd ../graywulf-entities
+git checkout -B v1.0/develop remotes/origin/v1.0/develop --
+
 cd ../footprint
 git checkout -B v2.0/develop remotes/origin/v2.0/develop --
 
