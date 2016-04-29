b1e70ae47527e19e41cfa2cddd775dbc68de493d
Laszlo Dobos <dobos@complex.elte.hu>
2016-04-29 15:18:23 +0200

    Merged down entities into develop.

--- checkout.sh
+++ checkout.sh
@@ -2,13 +2,13 @@
 #git clone --recursive --branch v2.0/develop git@github.com:sciserver/footprint-all.git
 
 cd graywulf
-git checkout -B v1.1/feature/entities remotes/origin/v1.1/feature/entities --
+git checkout -B v1.1/develop remotes/origin/v1.1/develop --
 
 cd ../graywulf-plugins
-git checkout -B v1.1/feature/entities remotes/origin/v1.1/feature/entities --
+git checkout -B v1.1/develop remotes/origin/v1.1/develop --
 
 cd ../footprint
-git checkout -B v2.0/feature/entities remotes/origin/v2.0/feature/entities --
+git checkout -B v2.0/develop remotes/origin/v2.0/develop --
 
 cd ../footprint-config
 git checkout -B v2.0/develop remotes/origin/v2.0/develop --
