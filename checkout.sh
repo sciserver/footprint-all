5355ecd0c2601ce9ebdc47ab3817231426858d6d
Laszlo Dobos <dobos@complex.elte.hu>
2016-04-29 15:10:01 +0200

    Merged in projects from graywulf-entities.

--- checkout.sh
+++ checkout.sh
@@ -2,16 +2,13 @@
 #git clone --recursive --branch v2.0/develop git@github.com:sciserver/footprint-all.git
 
 cd graywulf
-git checkout -B v1.1/develop remotes/origin/v1.1/develop --
+git checkout -B v1.1/feature/entities remotes/origin/v1.1/feature/entities --
 
 cd ../graywulf-plugins
-git checkout -B v1.1/develop remotes/origin/v1.1/develop --
-
-cd ../graywulf-entities
-git checkout -B v1.0/develop remotes/origin/v1.0/develop --
+git checkout -B v1.1/feature/entities remotes/origin/v1.1/feature/entities --
 
 cd ../footprint
-git checkout -B v2.0/develop remotes/origin/v2.0/develop --
+git checkout -B v2.0/feature/entities remotes/origin/v2.0/feature/entities --
 
 cd ../footprint-config
 git checkout -B v2.0/develop remotes/origin/v2.0/develop --
